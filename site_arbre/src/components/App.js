d1482bf2fe2ce78c996e4d7e997c469f4401a74e
Mahamat-Brahim-Abderassoul <mahamatbrahimabderassoul719@gmail.com>
2024-03-01 14:45:06 +0100

    l'avancement du projet dans la partie JSX et COMPONENTS

---
+++ site_arbre/src/components/App.js
@@ -0,0 +1,18 @@
+import Banner from "./Banner";
+import Cart from "./Cart";
+import ShoppingList from "./ShoppingList";
+
+function App() {
+  return (
+    
+    <div>
+      <Banner></Banner>
+      <Cart></Cart>
+      <ShoppingList></ShoppingList>
+    </div>
+    
+  );
+}
+
+
+export default App;
